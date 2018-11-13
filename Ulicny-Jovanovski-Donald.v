da46ba9549f59744db91d9006c85d438f8cf9425
mikeulicny <43107737+mikeulicny@users.noreply.github.com>
2018-11-13 17:02:54 -0500

    added missing semi-colons on lines 87 and 88

--- Ulicny-Jovanovski-Donald.v
+++ Ulicny-Jovanovski-Donald.v
@@ -84,8 +84,8 @@ module CPU (clock,PC, IR, ALUOut, MDR, Rs, Rt, reg8);
 				state = 4; // default next state
 				if (opcode == R_FORMAT) 
 					case (IR[5:0]) //case for the various R-type instructions
-						00: ALUOut = Rt << shamt //shift left logical
-						02: ALUOut = Rt >> shamt //shift right logical
+						00: ALUOut = Rt << shamt; //shift left logical
+						02: ALUOut = Rt >> shamt; //shift right logical
 						// 8: ALUOut = ...  	//jump register 
 						32: ALUOut = Rs + Rt; 	//add 
 						33: ALUOut = Rs - Rt; 	//subtract 
