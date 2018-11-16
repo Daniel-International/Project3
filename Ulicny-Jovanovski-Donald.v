fc3991dc257e012f0c130d8aa885cd23a4f72d5c
Daniel-International <38669778+Daniel-International@users.noreply.github.com>
2018-11-16 16:43:19 -0500

    Ulicny-Jovanovski-Donald
    Added Parameters JM and SWAP
    also added opcode for Store Byte

--- Ulicny-Jovanovski-Donald.v
+++ Ulicny-Jovanovski-Donald.v
@@ -1,7 +1,7 @@
 module CPU (clock,PC, IR, ALUOut, MDR, Rs, Rt, reg8);
 	parameter R_FORMAT		= 6'b000000;
 	parameter JUMP	   		= 6'b000010;
-	parameter JUMP_AND_LINK = 6'b000011;
+	parameter JUMP_AND_LINK 	= 6'b000011;
 	parameter BEQ 	   		= 6'b000100;
 	parameter BNE	   		= 6'b000101;
 	parameter ADDI	   		= 6'b001000;
@@ -9,9 +9,11 @@ module CPU (clock,PC, IR, ALUOut, MDR, Rs, Rt, reg8);
 	parameter ANDI			= 6'b001100;
 	parameter ORI			= 6'b001101;
 	parameter LW  	   		= 6'b100011;
-	parameter STORE_BYTE	= 6'b101000;
+	parameter STORE_BYTE		= 6'b101000;
 	parameter SW  	   		= 6'b101011;
-	//TODO: JM and SWAP instructions
+	parameter JM			= 6'b100101;
+	parameter SWAP			= 6'b100001;
+	//JM and SWAP instructions included
 	// other opcodes go here
 	//....
 	
@@ -154,7 +156,13 @@ module CPU (clock,PC, IR, ALUOut, MDR, Rs, Rt, reg8);
 					ALUOut <= Rs || SignExtend;
 
 				else if ((opcode == LW) |(opcode==SW)) 
-					ALUOut <= Rs + SignExtend; //compute effective address
+					ALUOut <= Rs + SignExtend;
+				
+				else if (opcode == JM)
+					ALUOut <= Rs + SignExtend;
+				
+				else if (opcode == SWAP)
+					//compute effective address
 
 
 			end	// state 3
@@ -175,7 +183,7 @@ module CPU (clock,PC, IR, ALUOut, MDR, Rs, Rt, reg8);
 				end	// immediate instructions
 
 				else if (opcode == STORE_BYTE) begin
-					// TODO
+					Memory[ALUOut>>2][7:0] = Rt[7:0]// TODO
 				end
 
 				else if (opcode == LW) begin // load instruction
